add05aa535acdefc1876b590b98c5ed28d74db88
Sanket Jadhav <jadhavsanket621@gmail.com>
2024-05-07 09:30:21 +0000

    push for firebase

--- src/Nav.js
+++ src/Nav.js
@@ -1,33 +1,39 @@
-import React, {useState,useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import "./Nav.css"
-
+import "./Nav.css";
 
 function Nav() {
-    const [show, handleShow] = useState(false);
-    const navigate = useNavigate();
+  const [show, handleShow] = useState(false);
+  const navigate = useNavigate();
 
-    const navbartransition = () => {
-        if (window.scrollY > 100) {
-            handleShow(true);
-        }else{
-            handleShow(false);
-        }
+  const navbartransition = () => {
+    if (window.scrollY > 100) {
+      handleShow(true);
+    } else {
+      handleShow(false);
     }
+  };
 
-    useEffect(() => {
-        window.addEventListener("scroll" , navbartransition);
-        return () => {
-            window.removeEventListener("scroll", navbartransition);
-        }
-    }, [])
+  useEffect(() => {
+    window.addEventListener("scroll", navbartransition);
+    return () => {
+      window.removeEventListener("scroll", navbartransition);
+    };
+  }, []);
 
-    return (
-        <div className={`nav ${show && "nav-dark"}`}>
-            <h1 onClick={() => navigate("/")} className='nav-logo'>filmflex</h1>
-            <img onClick={() => navigate("/profile")} className='user-avatar' src={require('./Components/media/avatar.png')} alt="" />
-        </div>
-    )
+  return (
+    <div className={`nav ${show && "nav-dark"}`}>
+      <h1 onClick={() => navigate("/")} className="nav-logo">
+        filmflex
+      </h1>
+      <img
+        onClick={() => navigate("/profile")}
+        className="user-avatar"
+        src={require("./Components/media/avatar.png")}
+        alt=""
+      />
+    </div>
+  );
 }
 
-export default Nav
+export default Nav;
